1a8c1e74314c5dac4e5c1d9734590044d8cbb6e1
l-const <const-l@outlook.com>
2021-04-20 01:33:32 +0300

    0.1.0 version

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # dhref
-An async url link scraper/downloader.
+**Download files embed to a page through relative/root-relative URLs.**
+
+### Description
+The program scrapes the url page specified for the hyperlink with   
+elative/root-relative URLs and  
+asynchronously downloads files filtered by the filetype specified in the input.
+
+### FileTypes
+* PDF
+* XLSX
+* DOCX
+* DOC
+* CSV
+* PPT
+* PPTX
+* ALL OF ABOVE
+
+### Examples
+
+```bash
+    dhref <url>  ftype pdf/PDF
+    dhref <url>  ftype csv/CSV
+    dhref <url>  -o ./ -f pdf
+    dhref <url>  -o ./out -f csv
+```
+
+### Help
+
+```bash
+    dhref --help
+
+    dhref 0.1.0
+    Kostas L. <konlampro94@gmail.com>
+    Download files embed in a page through
+        relative and root-relative hyperlinks.
+
+    USAGE:
+        dhref [OPTIONS] <uri>
+
+    FLAGS:
+        -h, --help       Prints help information
+        -V, --version    Prints version information
+
+    OPTIONS:
+        -f <ftype>          File suffix for the type of files to be searched( e.g pdf,doc,csv). (Optional)
+        -o <out_dir>        Relative path for the folder to place the output. (Optional)
+
+    ARGS:
+        <uri>    Http page url to be scraped. (Required)
+```
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
+extern crate clap;
 extern crate futures;
 extern crate nipper;
 extern crate reqwest;
 extern crate tokio;
-extern crate clap;
 
-
+use clap::{App, Arg};
 use futures::future;
 use nipper::Document;
-use std::env::Args;
+use std::convert::*;
 use std::error::Error;
 use std::fmt;
 use tokio::fs::File;
-use std::convert::*;
 use tokio::io::AsyncWriteExt;
-use clap::{Arg, App};
 
 struct ParseError<'uri> {
     uri: &'uri str,
 }
 
+#[derive(Debug, Default)]
+struct CliOpts<'input> {
+    page: &'input str,
+    out_dir: &'input str,
+    ftype: FileType,
+}
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum FileType {
     PDF,
     DOC,
@@ -29,37 +33,35 @@ enum FileType {
     CSV,
     PPT,
     PPTX,
-    ALL
+    ALL,
 }
-
 
 impl From<&str> for FileType {
     fn from(input: &str) -> Self {
         match input {
-            "pdf"  => FileType::PDF,
-            "doc"  => FileType::DOC,
+            "pdf" => FileType::PDF,
+            "doc" => FileType::DOC,
             "docx" => FileType::DOCX,
             "xlsx" => FileType::XLSX,
-            "ppt"  => FileType::PPT,
+            "ppt" => FileType::PPT,
             "pptx" => FileType::PPTX,
             "csv" => FileType::CSV,
-            _ => FileType::ALL
+            _ => FileType::ALL,
         }
     }
 }
-
 
 impl Into<&str> for FileType {
     fn into(self) -> &'static str {
         match self {
             FileType::PDF => ".pdf",
             FileType::DOC => ".doc",
-            FileType::DOCX  => ".docx",
+            FileType::DOCX => ".docx",
             FileType::XLSX => ".xlsx",
             FileType::PPT => ".ppt",
             FileType::PPTX => ".pptx",
             FileType::CSV => ".csv",
-            FileType::ALL => ""  
+            FileType::ALL => "",
         }
     }
 }
@@ -68,13 +70,6 @@ impl Default for FileType {
     fn default() -> Self {
         FileType::ALL
     }
-}
-
-#[derive(Debug, Default)]
-struct CliOpts<'input> {
-    page: &'input str,
-    out_dir: &'input str,
-    ftype: FileType
 }
 
 impl<'uri> Error for ParseError<'uri> {}
@@ -101,32 +96,40 @@ impl<'uri> fmt::Display for ParseError<'uri> {
 
 async fn download_one(location: &str) {
     let body = reqwest::get(location).await.unwrap().bytes().await.unwrap();
-    let mut file = File::create(location.split('/').last().unwrap())
-        .await
-        .unwrap();
-    file.write_all(&body).await.unwrap();
+    if let Ok(mut file) = File::create(location.split('/').last().unwrap()).await {
+        file.write_all(&body).await.unwrap();
+    }
 }
 
-fn parse_page(args: &mut Args, ftype: FileType) -> Vec<String> {
-    let base_uri = args.nth(1).unwrap();
-    check_url(&base_uri).unwrap();
-    let body = reqwest::blocking::get(base_uri.clone())
-        .unwrap()
-        .text()
-        .unwrap();
+#[tokio::main]
+async fn download_all(vector_path: Vec<String>) {
+    let futures: Vec<_> = vector_path.iter().map(|path| download_one(path)).collect();
+    future::join_all(futures).await;
+}
+
+fn parse_page(base_uri: &str, ftype: FileType) -> Option<Vec<String>> {
+    check_url(base_uri).unwrap();
+    let body = reqwest::blocking::get(base_uri).unwrap().text().unwrap();
     let document = Document::from(&body);
     let elements: Vec<String> = document
         .select("a")
         .iter()
         .map(|elem| elem.attr("href").unwrap().to_string())
         .filter(|elem_str| {
-            let ftype_str : &str = ftype.into();
-            elem_str.ends_with(ftype_str)
+            if ftype == FileType::ALL {
+                elem_str.contains(".")
+            } else {
+                let ftype_str: &str = ftype.into();
+                elem_str.ends_with(ftype_str)
+            }
         })
         .map(|elem| format!("{}{}", &base_uri, elem))
         .collect();
-    println!("Elements[1]: {}, len: {}", elements[1], elements.len());
-    elements
+    if elements.len() > 0 {
+        Some(elements)
+    } else {
+        None
+    }
 }
 
 fn check_url(url_str: &str) -> Result<(), ParseError> {
@@ -139,55 +142,35 @@ fn check_url(url_str: &str) -> Result<(), ParseError> {
 
 fn main() {
     let matches = App::new("dhref")
-                        .version("0.1.0")
-                        .author("Kostas L. <konlampro94@gmail.com>")
-                        .about("Download files embed in a page through\n relative and root-relative hyperlinks.")
-                        .arg(
-                            Arg::with_name("uri")
-                            .required(true)
-                            .takes_value(true)
-                            .help("Http page url to be scraped. (Required)")
-                        )
-                        .arg(
-                            Arg::with_name("out_dir")
-                                .takes_value(true)
-                                .help("Relative path for the folder to place the output. (Optional)"),
-                        )
-                        .arg(
-                            Arg::with_name("ftype")
-                                .takes_value(true)
-                                .help("File prefix for the output files for each table. (Optional)"),
-                        )
-                        .get_matches();
+        .version("0.1.0")
+        .author("Kostas L. <konlampro94@gmail.com>")
+        .about("Download files embed in a page through\n relative and root-relative hyperlinks.")
+        .arg(
+            Arg::with_name("uri")
+                .required(true)
+                .takes_value(true)
+                .help("Http page url to be scraped. (Required)"),
+        )
+        .arg(
+            Arg::with_name("out_dir")
+                .takes_value(true)
+                .short("o")
+                .help("Relative path for the folder to place the output. (Optional)"),
+        )
+        .arg(
+            Arg::with_name("ftype").short("f").takes_value(true).help(
+                "File suffix for the type of files to be searched( e.g pdf,doc,csv). (Optional)",
+            ),
+        )
+        .get_matches();
+
     let cli_opts = CliOpts {
-        page:  matches.value_of("uri").unwrap(),
-        out_dir : matches.value_of("out_dir").unwrap_or("./"),
-        ftype : matches.value_of("ftype").unwrap_or("").into()
+        page: &matches.value_of("uri").unwrap().to_ascii_lowercase(),
+        out_dir: matches.value_of("out_dir").unwrap_or("./"),
+        ftype: matches.value_of("ftype").unwrap_or("").into(),
     };
 
-}
-
-
-
-//TODO: CHECK FOR ABSOLUTE VS RELATIVE VS ROOT-RELATIVE URLS
-
-
-// fn main() {
-//     let mut args = std::env::args();
-//     if args.len() <= 1 {
-//         println!("Not enough arguments! Please specify a http/https uri!");
-//         return;
-//     } else if args.len() > 3 {
-//         println!("Too many arguments!\n");
-//         return;
-//     }
-//     let paths = parse_page(&mut args, ".pdf");
-//     download_all(paths);
-//     // let body = download("https://15445.courses.cs.cmu.edu/fall2019/slides/");
-// }
-
-#[tokio::main]
-async fn download_all(vector_path: Vec<String>) {
-    let futures: Vec<_> = vector_path.iter().map(|path| download_one(path)).collect();
-    future::join_all(futures).await;
+    if let Some(paths) = parse_page(cli_opts.page, cli_opts.ftype) {
+        download_all(paths)
+    }
 }
